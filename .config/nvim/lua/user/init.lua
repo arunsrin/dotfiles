a04ee1365eeb257a64580a610fe2c46489dc8bc4
Arun S <gnu.arun@gmail.com>
2025-04-10 12:02:40 +0530

    move to astronvim

---
+++ .config/nvim/lua/user/init.lua
@@ -0,0 +1,10 @@
+return {
+  -- Add any other user configurations here
+
+  plugins = {
+    "user.plugins", -- This tells Lazy to look for plugin files in the 'user/plugins' directory
+    -- You can add more plugin sources here if needed
+  },
+
+  -- Add any other configurations like key mappings, commands, etc.
+}
